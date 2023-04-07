d052652bc97905c7f2a28933ec722d4ade31b584
cw <spwolf147@gmail.com>
2023-04-07 20:41:47 +0900

    고객

--- src/main/java/com/itwillbs/movie/controller/CustomerController.java
+++ src/main/java/com/itwillbs/movie/controller/CustomerController.java
@@ -6,28 +6,38 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CustomerController {
-	
+
+
+	// 고객센터홈
 	@RequestMapping(value = "cc_home", method = {RequestMethod.GET, RequestMethod.POST})
 	public String ccHome() {
 		return "customer_center/cc_home";
 	}
+	// 자주묻는 게시판
 	@RequestMapping(value = "faq", method = {RequestMethod.GET, RequestMethod.POST})
 	public String faq() {
 		return "customer_center/faq";
 	}
-	@RequestMapping(value = "lost_boar", method = {RequestMethod.GET, RequestMethod.POST})
-	public String lostBoar() {
-		return "customer_center/lost_boar";
+	// 분실물 문의
+	@RequestMapping(value = "lost_board", method = {RequestMethod.GET, RequestMethod.POST})
+	public String lostBoard() {
+		return "customer_center/lost_board";
 	}
+	// 1대1문의
 	@RequestMapping(value = "oneOnOne", method = {RequestMethod.GET, RequestMethod.POST})
 	public String oneOnOne() {
 		return "customer_center/oneOnOne";
 	}
+	// 공지사항
 	@RequestMapping(value = "notice_board", method = {RequestMethod.GET, RequestMethod.POST})
 	public String noticeBoard() {
 		return "customer_center/notice_board";
 	}
-	
+	// 비회원문의내역
+	@RequestMapping(value = "guest_board", method = {RequestMethod.GET, RequestMethod.POST})
+	public String guestBoard() {
+		return "customer_center/guest_board";
+	}
 	
 	
 }
