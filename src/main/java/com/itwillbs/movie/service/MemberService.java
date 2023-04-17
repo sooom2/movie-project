2ed3a75830d5d23c87b0f9239dc1ba3728717cec
hej <hej@localhost>
2023-04-18 00:57:30 +0900

    Find Id/Pw FINISH

--- src/main/java/com/itwillbs/movie/service/MemberService.java
+++ src/main/java/com/itwillbs/movie/service/MemberService.java
@@ -3,10 +3,14 @@ package com.itwillbs.movie.service;
 import java.util.HashMap;
 import java.util.List;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.itwillbs.movie.mapper.MyBatisMapper;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 @Service
 public class MemberService {
@@ -58,6 +62,29 @@ public class MemberService {
 
 	public int insertPoint(String id) {
 		return mapper.insertPoint(id);
+	}
+	
+	// SMS 인증
+	public void certifiedPhoneNumber(String phone, int randomNumber) {
+		String api_key = "NCSQU2TAT8POKQ76";
+	    String api_secret = "N5KAV8MZDEGWQZYSQGJI27HQIQLB06VH";
+	    Message coolsms = new Message(api_key, api_secret);
+
+	    HashMap<String, String> params = new HashMap<String, String>();
+	    params.put("to", phone);    // 수신전화번호
+	    params.put("from", "01076110230");    // 발신전화번호
+	    params.put("type", "SMS");
+	    params.put("text", "[아이무비] 인증번호는" + "["+randomNumber+"]" + "입니다."); // 문자 내용 입력
+	    params.put("app_version", "test app 1.2"); 
+
+	    try {
+	        JSONObject obj = (JSONObject)coolsms.send(params);
+	        System.out.println(obj.toString());
+	      } catch (CoolsmsException e) {
+	        System.out.println(e.getMessage());
+	        System.out.println(e.getCode());
+	      }
+		
 	}
 
 
